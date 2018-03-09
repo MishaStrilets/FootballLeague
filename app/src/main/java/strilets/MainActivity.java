9624a35d1b585149c4163d52c21286d04c5d9070
Misha Strilets <mishastrilets@gmail.com>
2018-03-09 12:02:10 +0200

    add icon

--- app/src/main/java/strilets/MainActivity.java
+++ app/src/main/java/strilets/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.title_activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
